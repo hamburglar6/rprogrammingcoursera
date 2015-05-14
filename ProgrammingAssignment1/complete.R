033b675f3877339c65eba8c141e7a2e0efc94b90
Kevin Lyons <hamburglar6@gmail.com>
2015-05-14 01:39:37 -0400

    finished programming assignment 1

--- ProgrammingAssignment1/complete.R
+++ ProgrammingAssignment1/complete.R
@@ -1,6 +1,5 @@
 complete <- function(directory, id = 1:332) {
     data_frame <- data.frame(id = integer(0), nobs = integer(0))
-    colnames(data_frame) <- c('id', 'nobs')
     filenames <- paste(formatC(id, width = 3, format = 'd', flag = '0'), 'csv', sep = '.')
     filenames <- paste(directory, filenames, sep = '/')
     for (i in 1:length(id)) {
@@ -9,5 +8,6 @@ complete <- function(directory, id = 1:332) {
         new_column <- c(id[i], sum(indices))
         data_frame <- rbind(data_frame, new_column)
     }
+    colnames(data_frame) <- c('id', 'nobs')
     data_frame
 }
